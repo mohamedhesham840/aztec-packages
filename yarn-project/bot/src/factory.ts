mohamedhesham840/aztec-packages

commit 6739214b2a9ff0780e3de9ce3415052487600cbc

feat!: enforce fees (#11480)
### Circuits
- Private kernels ensure that fee payer is not empty.
- Private base rollup and the avm check that the fee payer must have
enough balance to pay the fee.

### Aztec.js/cli
- Default payment method is `FeeJuicePaymentMethod`, the fee payer is
the wallet owner.
- `NoFeePaymentMethod` is no longer available.
-

### End-to-end/Sandbox
- Some public data leaves are created for funding the initial test
accounts with fee juice. The genesis archive root and block hash are
generated with these public data leaves.
  - For the e2e tests, the test accounts are generated randomly.
- For the sandbox, the test accounts are defined in
`@aztec/accounts/testing`.
- These funded test accounts can deploy their own account contract and
pay the fee for the deployment themselves.
- These funded test accounts can be used to deploy another account
without pre-funded fee juice.
- By calling `someAccountManager.deploy({ deployWallet: fundedWallet })`
- `BananaCoin` and `BananaFPC` are deployed in sandbox by default. Users
can use the funded accounts to mint banana coin for a new account.
- The new account can then submit transactions and pay the fees using
`PrivateFeePaymentMethod`.
  - See example in `end-to-end/src/composed/e2e_sandbox_example.test.ts`

---------

Co-authored-by: Alex Gherghisan <alexghr@users.noreply.github.com>
Co-authored-by: ludamad <domuradical@gmail.com>

yarn-project/bot/src/factory.ts
@@ -1,14 +1,19 @@
 import { getSchnorrAccount } from '@aztec/accounts/schnorr';
+import { getDeployedTestAccountsWallets, getInitialTestAccounts } from '@aztec/accounts/testing';
 import {
   type AccountWallet,
   BatchCall,
   type DeployMethod,
   type DeployOptions,
+  FeeJuicePaymentMethodWithClaim,
+  L1FeeJuicePortalManager,
   createLogger,
   createPXEClient,
+  retryUntil,
 } from '@aztec/aztec.js';
 import { type AztecNode, type FunctionCall, type PXE } from '@aztec/circuit-types';
-import { Fr, deriveSigningKey } from '@aztec/circuits.js';
+import { type AztecAddress, Fr, deriveSigningKey } from '@aztec/circuits.js';
+import { createEthereumChain, createL1Clients } from '@aztec/ethereum';
 import { EasyPrivateTokenContract } from '@aztec/noir-contracts.js/EasyPrivateToken';
 import { TokenContract } from '@aztec/noir-contracts.js/Token';
 import { makeTracedFetch } from '@aztec/telemetry-client';
@@ -27,6 +32,11 @@ export class BotFactory {
   constructor(private readonly config: BotConfig, dependencies: { pxe?: PXE; node?: AztecNode } = {}) {
     if (config.flushSetupTransactions && !dependencies.node) {
       throw new Error(`Either a node client or node url must be provided if transaction flushing is requested`);
+    }
+    if (config.senderPrivateKey && !dependencies.node) {
+      throw new Error(
+        `Either a node client or node url must be provided for bridging L1 fee juice to deploy an account with private key`,
+      );
     }
     if (!dependencies.pxe && !config.pxeUrl) {
       throw new Error(`Either a PXE client or a PXE URL must be provided`);
@@ -60,17 +70,30 @@ export class BotFactory {
    * @returns The sender wallet.
    */
   private async setupAccount() {
+    if (this.config.senderPrivateKey) {
+      return await this.setupAccountWithPrivateKey(this.config.senderPrivateKey);
+    } else {
+      return await this.setupTestAccount();
+    }
+  }
+
+  private async setupAccountWithPrivateKey(privateKey: Fr) {
     const salt = Fr.ONE;
-    const signingKey = deriveSigningKey(this.config.senderPrivateKey);
-    const account = await getSchnorrAccount(this.pxe, this.config.senderPrivateKey, signingKey, salt);
+    const signingKey = deriveSigningKey(privateKey);
+    const account = await getSchnorrAccount(this.pxe, privateKey, signingKey, salt);
     const isInit = (await this.pxe.getContractMetadata(account.getAddress())).isContractInitialized;
     if (isInit) {
       this.log.info(`Account at ${account.getAddress().toString()} already initialized`);
       const wallet = await account.register();
       return wallet;
     } else {
-      this.log.info(`Initializing account at ${account.getAddress().toString()}`);
-      const sentTx = account.deploy();
+      const address = account.getAddress();
+      this.log.info(`Deploying account at ${address}`);
+
+      const claim = await this.bridgeL1FeeJuice(address, 10n ** 22n);
+
+      const paymentMethod = new FeeJuicePaymentMethodWithClaim(address, claim);
+      const sentTx = account.deploy({ fee: { paymentMethod } });
       const txHash = await sentTx.getTxHash();
       this.log.info(`Sent tx with hash ${txHash.toString()}`);
       if (this.config.flushSetupTransactions) {
@@ -79,8 +102,23 @@ export class BotFactory {
       }
       this.log.verbose('Waiting for account deployment to settle');
       await sentTx.wait({ timeout: this.config.txMinedWaitSeconds });
+      this.log.info(`Account deployed at ${address}`);
       return account.getWallet();
     }
+  }
+
+  private async setupTestAccount() {
+    let [wallet] = await getDeployedTestAccountsWallets(this.pxe);
+    if (wallet) {
+      this.log.info(`Using funded test account: ${wallet.getAddress()}`);
+    } else {
+      this.log.info('Registering funded test account');
+      const [account] = await getInitialTestAccounts();
+      const manager = await getSchnorrAccount(this.pxe, account.secret, account.signingKey, account.salt);
+      wallet = await manager.register();
+      this.log.info(`Funded test account registered: ${wallet.getAddress()}`);
+    }
+    return wallet;
   }
 
   /**
@@ -173,5 +211,38 @@ export class BotFactory {
     }
     this.log.verbose('Waiting for token mint to settle');
     await sentTx.wait({ timeout: this.config.txMinedWaitSeconds });
+  }
+
+  private async bridgeL1FeeJuice(recipient: AztecAddress, amount: bigint) {
+    const l1RpcUrl = this.config.l1RpcUrl;
+    if (!l1RpcUrl) {
+      throw new Error('L1 Rpc url is required to bridge the fee juice to fund the deployment of the account.');
+    }
+    const mnemonicOrPrivateKey = this.config.l1Mnemonic || this.config.l1PrivateKey;
+    if (!mnemonicOrPrivateKey) {
+      throw new Error(
+        'Either a mnemonic or private key of an L1 account is required to bridge the fee juice to fund the deployment of the account.',
+      );
+    }
+
+    const { l1ChainId } = await this.pxe.getNodeInfo();
+    const chain = createEthereumChain(l1RpcUrl, l1ChainId);
+    const { publicClient, walletClient } = createL1Clients(chain.rpcUrl, mnemonicOrPrivateKey, chain.chainInfo);
+
+    const portal = await L1FeeJuicePortalManager.new(this.pxe, publicClient, walletClient, this.log);
+    const claim = await portal.bridgeTokensPublic(recipient, amount, true /* mint */);
+    this.log.info('Created a claim for L1 fee juice.');
+
+    // Progress by 2 L2 blocks so that the l1ToL2Message added above will be available to use on L2.
+    await this.advanceL2Block();
+    await this.advanceL2Block();
+
+    return claim;
+  }
+
+  private async advanceL2Block() {
+    const initialBlockNumber = await this.node!.getBlockNumber();
+    await this.node!.flushTxs();
+    await retryUntil(async () => (await this.node!.getBlockNumber()) >= initialBlockNumber + 1);
   }
 }

yarn-project/prover-node/src/factory.ts
@@ -1,6 +1,7 @@
 import { type Archiver, createArchiver } from '@aztec/archiver';
 import { type BlobSinkClientInterface, createBlobSinkClient } from '@aztec/blob-sink/client';
 import { type ProverCoordination, type ProvingJobBroker } from '@aztec/circuit-types';
+import { type PublicDataTreeLeaf } from '@aztec/circuits.js';
 import { EpochCache } from '@aztec/epoch-cache';
 import { L1TxUtils, RollupContract, createEthereumChain, createL1Clients } from '@aztec/ethereum';
 import { type Logger, createLogger } from '@aztec/foundation/log';
@@ -29,6 +30,9 @@ export async function createProverNode(
     broker?: ProvingJobBroker;
     l1TxUtils?: L1TxUtils;
   } = {},
+  options: {
+    prefilledPublicData?: PublicDataTreeLeaf[];
+  } = {},
 ) {
   const telemetry = deps.telemetry ?? getTelemetryClient();
   const blobSinkClient = deps.blobSinkClient ?? createBlobSinkClient(config);
@@ -37,7 +41,12 @@ export async function createProverNode(
   log.verbose(`Created archiver and synced to block ${await archiver.getBlockNumber()}`);
 
   const worldStateConfig = { ...config, worldStateProvenBlocksOnly: false };
-  const worldStateSynchronizer = await createWorldStateSynchronizer(worldStateConfig, archiver, telemetry);
+  const worldStateSynchronizer = await createWorldStateSynchronizer(
+    worldStateConfig,
+    archiver,
+    options.prefilledPublicData,
+    telemetry,
+  );
   await worldStateSynchronizer.start();
 
   const broker = deps.broker ?? (await createAndStartProvingBroker(config, telemetry));

yarn-project/world-state/src/synchronizer/factory.ts
@@ -1,4 +1,5 @@
 import { type L1ToL2MessageSource, type L2BlockSource } from '@aztec/circuit-types';
+import { type PublicDataTreeLeaf } from '@aztec/circuits.js';
 import { type DataStoreConfig } from '@aztec/kv-store/config';
 import { type TelemetryClient, getTelemetryClient } from '@aztec/telemetry-client';
 
@@ -10,16 +11,18 @@ import { ServerWorldStateSynchronizer } from './server_world_state_synchronizer.
 export async function createWorldStateSynchronizer(
   config: WorldStateConfig & DataStoreConfig,
   l2BlockSource: L2BlockSource & L1ToL2MessageSource,
+  prefilledPublicData: PublicDataTreeLeaf[] = [],
   client: TelemetryClient = getTelemetryClient(),
 ) {
   const instrumentation = new WorldStateInstrumentation(client);
-  const merkleTrees = await createWorldState(config, instrumentation);
+  const merkleTrees = await createWorldState(config, prefilledPublicData, instrumentation);
   return new ServerWorldStateSynchronizer(merkleTrees, l2BlockSource, config, instrumentation);
 }
 
 export async function createWorldState(
   config: WorldStateConfig & DataStoreConfig,
-  instrumentation: WorldStateInstrumentation,
+  prefilledPublicData: PublicDataTreeLeaf[] = [],
+  instrumentation: WorldStateInstrumentation = new WorldStateInstrumentation(getTelemetryClient()),
 ) {
   const newConfig = {
     dataDirectory: config.worldStateDataDirectory ?? config.dataDirectory,
@@ -36,11 +39,13 @@ export async function createWorldState(
         config.l1Contracts.rollupAddress,
         newConfig.dataDirectory,
         newConfig.dataStoreMapSizeKB,
+        prefilledPublicData,
         instrumentation,
       )
     : await NativeWorldStateService.tmp(
         config.l1Contracts.rollupAddress,
         !['true', '1'].includes(process.env.DEBUG_WORLD_STATE!),
+        prefilledPublicData,
       );
 
   return merkleTrees;

commit 098c06564614b36b1d13ca71a1f96fb574982f8e

fix: Do not try flushing txs in bot setup if not set (#12144)
Adds a missing guard to flush txs that caused the bot to fail setup in
public networks, accidentally introduced in #11480.

yarn-project/bot/src/factory.ts
@@ -97,10 +97,7 @@ export class BotFactory {
       const sentTx = account.deploy({ fee: { paymentMethod } });
       const txHash = await sentTx.getTxHash();
       this.log.info(`Sent tx with hash ${txHash.toString()}`);
-      if (this.config.flushSetupTransactions) {
-        this.log.verbose('Flushing transactions');
-        await this.node!.flushTxs();
-      }
+      await this.tryFlushTxs();
       this.log.verbose('Waiting for account deployment to settle');
       await sentTx.wait({ timeout: this.config.txMinedWaitSeconds });
       this.log.info(`Account deployed at ${address}`);
@@ -159,10 +156,7 @@ export class BotFactory {
       const sentTx = deploy.send(deployOpts);
       const txHash = await sentTx.getTxHash();
       this.log.info(`Sent tx with hash ${txHash.toString()}`);
-      if (this.config.flushSetupTransactions) {
-        this.log.verbose('Flushing transactions');
-        await this.node!.flushTxs();
-      }
+      await this.tryFlushTxs();
       this.log.verbose('Waiting for token setup to settle');
       return sentTx.deployed({ timeout: this.config.txMinedWaitSeconds });
     }
@@ -206,10 +200,7 @@ export class BotFactory {
     const sentTx = new BatchCall(token.wallet, calls).send();
     const txHash = await sentTx.getTxHash();
     this.log.info(`Sent tx with hash ${txHash.toString()}`);
-    if (this.config.flushSetupTransactions) {
-      this.log.verbose('Flushing transactions');
-      await this.node!.flushTxs();
-    }
+    await this.tryFlushTxs();
     this.log.verbose('Waiting for token mint to settle');
     await sentTx.wait({ timeout: this.config.txMinedWaitSeconds });
   }
@@ -243,7 +234,18 @@ export class BotFactory {
 
   private async advanceL2Block() {
     const initialBlockNumber = await this.node!.getBlockNumber();
-    await this.node!.flushTxs();
+    await this.tryFlushTxs();
     await retryUntil(async () => (await this.node!.getBlockNumber()) >= initialBlockNumber + 1);
+  }
+
+  private async tryFlushTxs() {
+    if (this.config.flushSetupTransactions) {
+      this.log.verbose('Flushing transactions');
+      try {
+        await this.node!.flushTxs();
+      } catch (err) {
+        this.log.error(`Failed to flush transactions: ${err}`);
+      }
+    }
   }
 }